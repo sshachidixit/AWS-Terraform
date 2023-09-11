d9137fcc53064a6727b7ed28d4e5b910975d336c
sshachidixit <sshachidixit@gmail.com>
2023-09-11 14:37:25 +0530

    add ec2

---
+++ Modules/Ec2/ec2.tf
@@ -0,0 +1,15 @@
+#Varibale declaration
+
+variable "ec2name" {
+    type = string
+  
+}
+
+
+resource "aws_instance" "ec2" {
+    ami = "ami-02bb7d8191b50f4bb"
+    instance_type = "t2.micro"
+    tags = {
+      Name =var.ec2name
+    }
+}
