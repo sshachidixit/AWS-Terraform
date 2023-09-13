22915e4363492e2ffadadf107c7688432c543257
sshachidixit <117880753+sshachidixit@users.noreply.github.com>
2023-09-13 22:26:29 +0530

    Adding basic eip terraform code

---
+++ Eip/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+    region = "ap-south-1"
+  
+}
+
+resource "aws_instance" "ec2" {
+    ami = "ami-02bb7d8191b50f4bb"
+    instance_type = "t2.micro"
+  
+  }
+
+  resource "aws_eip" "eip" {
+    instance = aws_eip.eip.id
+    
+  }
+
+  output "eip_public_ip" {
+    value = aws_eip.eip.public_ip
+    
+  }
